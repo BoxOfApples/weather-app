7ee086e072ccf1fee0fa635f8d5f4419f213c2a5
BoxOfApples <105881470+BoxOfApples@users.noreply.github.com>
2025-10-04 19:29:14 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 import requests
 A = "48534e6daec4966aae103210c01f918f"
-c = input("Введмте город: ")
+c = input("Enter the city: ")
 u = f"http://api.openweathermap.org/data/2.5/weather?q={c}&appid={A}&units=metric"
 r = requests.get(u)
 if r.status_code == 200:
     d = r.json()
     t = d["main"]["temp"]
     w = d["weather"][0]["description"]
-    print(f"Температура в {c}:{t}")
-    print(f"Погода {w}")
+    print(f"Тtemperature {c}:{t}")
+    print(f"Weather {w}")
 else:
-    print("ошибка")
+    print("error")
