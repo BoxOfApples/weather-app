a5a20fb332a9326458687e2663a7046336159f03
BoxOfApples <105881470+BoxOfApples@users.noreply.github.com>
2025-10-04 19:34:34 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,14 +1,14 @@
 git status
 import requests
 A = "48534e6daec4966aae103210c01f918f"
-c = input("Введмте город: ")
+c = input("Podaj miasto: ")
 u = f"http://api.openweathermap.org/data/2.5/weather?q={c}&appid={A}&units=metric"
 r = requests.get(u)
 if r.status_code == 200:
     d = r.json()
     t = d["main"]["temp"]
     w = d["weather"][0]["description"]
-    print(f"Температура в {c}:{t}")
+    print(f"Temperatura w {c}:{t}")
     print(f"Погода {w}")
 else:
-    print("ошибка")
+    print("Błąd podczas pobierania danych.")
